bef738bfec3ed8b9bc71678984d52af99670c64b
Cameron Preito <cameotheman@yahoo.com>
2014-11-20 18:22:57 -0800

    video 12

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -18,7 +18,13 @@ game.PlayerEntity = me.Entity.extend({
         this.renderable.setCurrentAnimation("idle");
                
        this.body.setVelocity(5, 20);
-        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+       me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+       
+       this.body.setVelocity(5, 20);
+       me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+       
+       this.body.setVelocity(5, 20);
+       me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
         
     },
     
@@ -28,10 +34,31 @@ game.PlayerEntity = me.Entity.extend({
            
        }else{
            this.body.vel.x = 0;
+       }   
+           
+       if(me.input.isKeyPressed("left")){
+           this.body.vel.x -= this.body.accel.x / me.timer.tick;
+       }else{
+           this.body.vel.x = 0;
        }
        
+       if(me.input.isKeyPressed("up")){
+           this.body.vel.x += this.body.accel.x * me.timer.tick;
+           
+       }else{
+           this.body.vel.x = 0;
+       }
+       
+       if(me.input.isKeyPressed("up")){
+           this.body.vel.x += this.body.accel.x * me.timer.tick;
+           
+       }else{
+           this.body.vel.x = 0;
+       }
+           
        this.body.update(delta);  
        me.collision.check(this, true, this.collideHandler.bind(this), true);
+       }
        
        if(this.body.vel.x !== 0){
             if(!this.renderable.isCurrentAnimation("smallWalk")) {
