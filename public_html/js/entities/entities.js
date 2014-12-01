ff94567c935142c5a6745fc9a65d1c95b8b81ab2
Cameron Preito <cameotheman@yahoo.com>
2014-12-01 15:03:44 -0800

    video 17

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -29,16 +29,14 @@ game.PlayerEntity = me.Entity.extend({
        if(me.input.isKeyPressed("right")){
            this.body.vel.x += this.body.accel.x * me.timer.tick;
            
-       }else{
-           this.body.vel.x = 0;
-       }   
-           
-       if(me.input.isKeyPressed("left")){
+       }else if(me.input.isKeyPressed("left")){
            this.body.vel.x -= this.body.accel.x / me.timer.tick;
        
-       }else{
+       }      
+        else{
            this.body.vel.x = 0;
-       }            
+       }   
+
        
        this.body.update(delta);  
        me.collision.check(this, true, this.collideHandler.bind(this), true);
