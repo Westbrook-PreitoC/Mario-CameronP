835311f16d3ea255faa7a14c4d13ede95c221be2
Cameron Preito <cameotheman@yahoo.com>
2014-12-03 17:07:29 -0800

    good job so far

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -134,29 +134,28 @@ game.BadGuy = me.Entity.extend({
     },
     
     update: function(delta){
-        
-        // updates character animation and checks collisions
-//        this.body.update(delta);
-//        me.collision.check(this, true, this.collideHandler.bind(this), true);
-//        
-//        
-//        //
-//        if(this.alive){
-//            if(this.walkLeft && this.pos.x <= this.startX){
-//                this.walkLeft = false;
-//            }else if(!this.walkLeft && this.pos.x >= this.endX){
-//                this.walkLeft = true;
-//            }
-//            this.flipX(!this.walkLeft);
-//            this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick : this.body.accel.x * me.timer.tick;
-//            
-//        }else{
-//            me.game.world.removeChild(this);
-//        }
-//        
-//        
-//        this._super(me.Entity, "update", [delta]);
-//        return true;
+
+            // updates character animation and checks collisions
+        this.body.update(delta);
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
+
+
+        if (this.alive) {
+            if (this.walkLeft && this.pos.x <= this.startX) {
+                this.walkLeft = false;
+            } else if (!this.walkLeft && this.pos.x >= this.endX) {
+                this.walkLeft = true;
+            }
+            this.flipX(!this.walkLeft);
+            this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick : this.body.accel.x * me.timer.tick;
+
+        } else {
+            me.game.world.removeChild(this);
+        }
+
+
+        this._super(me.Entity, "update", [delta]);
+        return true;
     },
     
     collideHandler: function(){
